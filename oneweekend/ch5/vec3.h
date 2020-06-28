c63237bd1ddf5b60d513621f1cb7bb8e33f6b7d7
Chris O'Dell <christine.ann.odell@gmail.com>
2020-06-28 22:35:50 +0100

    Fix bugs in the vec3 header 🤦‍♀️️

--- oneweekend/ch5/vec3.h
+++ oneweekend/ch5/vec3.h
@@ -82,46 +82,46 @@ inline vec3 cross(const vec3 &v1, const vec3 &v2) {
 }
 
 inline vec3& vec3::operator+=(const vec3 &v){
-    e[0] + v.e[0];
-    e[1] + v.e[1];
-    e[2] + v.e[2];
+    e[0] += v.e[0];
+    e[1] += v.e[1];
+    e[2] += v.e[2];
     return *this;
 }
 
 inline vec3& vec3::operator*=(const vec3 &v){
-    e[0] * v.e[0];
-    e[1] * v.e[1];
-    e[2] * v.e[2];
+    e[0] *= v.e[0];
+    e[1] *= v.e[1];
+    e[2] *= v.e[2];
     return *this;
 }
 
 inline vec3& vec3::operator/=(const vec3 &v){
-    e[0] / v.e[0];
-    e[1] / v.e[1];
-    e[2] / v.e[2];
+    e[0] /= v.e[0];
+    e[1] /= v.e[1];
+    e[2] /= v.e[2];
     return *this;
 }
 
 inline vec3& vec3::operator-=(const vec3 &v){
-    e[0] - v.e[0];
-    e[1] - v.e[1];
-    e[2] - v.e[2];
+    e[0] -= v.e[0];
+    e[1] -= v.e[1];
+    e[2] -= v.e[2];
     return *this;
 }
 
 inline vec3& vec3::operator*=(const float t){
-    e[0] * t;
-    e[1] * t;
-    e[2] * t;
+    e[0] *= t;
+    e[1] *= t;
+    e[2] *= t;
     return *this;
 }
 
 inline vec3& vec3::operator/=(const float t){
     float k = 1.0/t;
 
-    e[0] * t;
-    e[1] * t;
-    e[2] * t;
+    e[0] *= k;
+    e[1] *= k;
+    e[2] *= k;
     return *this;
 }
 
